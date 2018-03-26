ab52d51c45fa2be44ca022b989e4a0ac065c9ccb
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-03-26 15:37:09 +0900

    add

--- How many finger/ViewController.swift
+++ How many finger/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func guess(_ sender: Any) {
+        //랜덤 숫자 생성
+        let number = arc4random() % 6
+        print(number)
+        let numberString = String(number)
+        
+        if input.text == numberString {
+            resultLabel.text = "정답 ㅎ"
+        } else {
+            resultLabel.text = "틀림 ㅋ 정답은\(number)"
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true)
+    input.backgroundColor = UIColor.green
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        input.resignFirstResponder()
+        view.backgroundColor = UIColor.yellow
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
